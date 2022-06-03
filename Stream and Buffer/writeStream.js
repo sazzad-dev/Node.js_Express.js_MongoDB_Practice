9bbe92338116cda4c13cb00a687f6e052d486fe8
shb011 <sazzad.bhuiyan011@gmail.com>
2022-06-03 18:42:32 +0600

    write stream

---
+++ Stream and Buffer/writeStream.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+const fs = require('fs');
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+// file write
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
+});
+
+// another way of file write
+ourReadStream.pipe(ourWriteStream);
+
+// file write in a server
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const myReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
+    myReadStream.pipe(res);
+});
+server.listen(5000);
